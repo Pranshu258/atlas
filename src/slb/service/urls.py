501b2813df30668fec8b6cd1ca815248ffb2169d
Pranshu Gupta <pranshug258@outlook.com>
2024-12-08 00:09:43 -0800

    added more url patterns

--- src/slb/service/urls.py
+++ src/slb/service/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from service import views
 
 urlpatterns = [
-    path("", views.load_balance, name="load_balance"),
+    path("/register", views.register, name="register"),
+    # catch-all pattern for request forwarding (must be last in the list)
+    re_path(r'^(?P<path>.*)$', views.forward, name="forward")
 ]
--- src/slb/service/views.py
+++ src/slb/service/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
-def load_balance(request):
+def forward(request):
     return HttpResponse("Hi, welcome to the frontdoor, slb service is up and running")
