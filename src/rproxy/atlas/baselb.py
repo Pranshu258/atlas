4508c93adc0b32250477f4d6f423e2d036ccacc5
Pranshu Gupta <pranshug258@outlook.com>
2024-12-11 13:50:13 +0530

    added lb classes

---
+++ src/rproxy/atlas/baselb.py
@@ -0,0 +1,10 @@
+import threading
+
+class BaseLoadBalancer: 
+    def __init__(self, servers):
+        self.servers = servers
+        self.lock = threading.Lock()
+
+    def add_server(self, server):
+        with self.lock:
+            self.servers.append(server)
---
+++ src/rproxy/atlas/randomlb.py
@@ -0,0 +1,9 @@
+import random
+from baselb import BaseLoadBalancer
+
+class RandomLoadBalancer(BaseLoadBalancer):
+    def __init__(self, servers):
+        super().__init__(servers)
+        
+    def get_next_server(self):
+        return random.choice(self.servers)
