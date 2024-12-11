b3b5398f1779a6d9071bedc29a3a7ed1dc113f73
Pranshu Gupta <pranshug258@outlook.com>
2024-12-11 14:08:36 +0530

    adding server working now

--- src/rproxy/server/apps.py
+++ src/rproxy/server/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
-
+import atlas.loadbalancerfactory as LoadBalancerFactory
 
 class ServerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'server'
+    loadbalancer = LoadBalancerFactory.LoadBalancerFactory().CreateLoadBalancer(
+        {'lb_type': 'roundrobin', 'servers': ['127.0.0.1:8001', '127.0.0.1:8002']}
+    )
--- src/rproxy/server/views.py
+++ src/rproxy/server/views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
+import atlas.apps as Atlas
+from .apps import ServerConfig
 
 def register(request):
-    return HttpResponse("Hello, Django!")
+    message = Atlas.AtlasConfig.name
+    ServerConfig.loadbalancer.add_server("127.0.0.1:8003")
+    return HttpResponse("Hello! " + message)
