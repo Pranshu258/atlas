2ddb2930e2a237944ac2bee7b218714f3d9048f8
Pranshu Gupta <pranshug258@outlook.com>
2024-12-11 13:13:22 +0530

    reset

--- src/slb/atlaslb/randomlb.py
+++
@@ -1,9 +0,0 @@
-import random
-from baselb import BaseLoadBalancer
-
-class RandomLoadBalancer(BaseLoadBalancer):
-    def __init__(self, servers):
-        super().__init__(servers)
-        
-    def get_next_server(self):
-        return random.choice(self.servers)
--- src/slb/service/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from apps import loadbalancer
-
-def register(request):
-    message = loadbalancer.add_server("randomhostname")
-    return HttpResponse(message)
-
-def forward(request, path):
-    return HttpResponse("Hi, welcome to the frontdoor, slb service is up and running")
