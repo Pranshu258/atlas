37a2aa0684983d09d505fb53987ac5bc29918aad
Pranshu Gupta <pranshug258@outlook.com>
2024-12-11 11:35:50 +0530

    added lb factory

---
+++ src/slb/atlaslb/loadbalancerfactory.py
@@ -0,0 +1,9 @@
+from roundrobinlb import RoundRobinLoadBalancer
+from randomlb import RandomLoadBalancer
+
+class LoadBalancerFactory: 
+    def CreateLoadBalancer(self, configuration):
+        if configuration['lb_type'] == 'roundrobin':
+            return RoundRobinLoadBalancer(configuration['servers'])
+        else:
+            return RandomLoadBalancer(configuration['servers'])
--- src/slb/service/apps.py
+++ src/slb/service/apps.py
@@ -1,6 +1,13 @@
 from django.apps import AppConfig
 
-
 class ServiceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'service'
+    configuration = {
+        'lb_type': 'roundrobin',
+        'servers': ['localhost:8000', 'localhost:8001', 'localhost:8002']
+    }
+
+    def ready(self):
+        from slb.atlaslb.loadbalancerfactory import LoadBalancerFactory
+        loadbalancer = LoadBalancerFactory.CreateLoadBalancer(self, self.configuration)
