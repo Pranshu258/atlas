887cd59df43ea663927c03070fc82e4c149e5d50
Pranshu Gupta <pranshug258@outlook.com>
2024-12-12 08:40:37 +0530

    added weighted round robin implementation

--- src/rproxy/atlas/loadbalancerfactory.py
+++ src/rproxy/atlas/loadbalancerfactory.py
@@ -1,9 +1,15 @@
 from .roundrobinlb import RoundRobinLoadBalancer
 from .randomlb import RandomLoadBalancer
+from .weightedroundrobinlb import WeightedRoundRobinLoadBalancer
 
 class LoadBalancerFactory: 
     def CreateLoadBalancer(self, configuration):
-        if configuration['lb_type'] == 'roundrobin':
-            return RoundRobinLoadBalancer(configuration['servers'])
-        else:
-            return RandomLoadBalancer(configuration['servers'])
+        match configuration['lb_type']:
+            case 'random':
+                return RandomLoadBalancer(configuration['servers'])
+            case 'roundrobin':
+                return RoundRobinLoadBalancer(configuration['servers'])
+            case 'weightedroundrobin':
+                return WeightedRoundRobinLoadBalancer(configuration['servers'], weights=configuration['weights'])
+            case _:
+                raise ValueError(f"Unknown load balancer type: {configuration['lb_type']}")
