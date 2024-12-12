fd9993d7686dce856d6e450f45de3d81ce0aec63
Pranshu Gupta <pranshug258@outlook.com>
2024-12-12 08:48:45 +0530

    updated atlas api path

--- src/rproxy/server/urls.py
+++ src/rproxy/server/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, re_path
 from server import views
 
 urlpatterns = [
-    path("register/", views.register, name="register"),
+    path("atlas/register/", views.register, name="register"),
     # catch-all pattern for request forwarding (must be last in the list)
     re_path(r'^(?P<path>.*)$', views.forward, name="forward")
 ]
