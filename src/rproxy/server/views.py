3df4a7750c765923c3c31e2d3e5a642dc51472bd
Pranshu Gupta <pranshug258@outlook.com>
2024-12-13 15:49:50 +0530

    simplified server addition

--- src/rproxy/server/views.py
+++ src/rproxy/server/views.py
@@ -6,16 +6,8 @@ from atlas.server import OriginServer
 
 @require_http_methods(["POST"])
 async def register(request):
-    try:
-        data = json.loads(request.body)
-        server = data.get('origin_server')
-        if server:
-            server = OriginServer(**server)
-        if not server:
-            return HttpResponse("Invalid request - Origin host name not provided.", status=400)
-    except Exception as e:
-        return HttpResponse(f"Error reading request body: {str(e)}", status=400)
-    print(f"Registering server: {server.host}, with weight {server.weight}")
+    data = json.loads(request.body)
+    server = OriginServer(host=data.get('host'), weight=data.get('weight'))
     await ServerConfig.loadbalancer.add_server(server)
     return HttpResponse("Current State: " + str(ServerConfig.loadbalancer))
 
