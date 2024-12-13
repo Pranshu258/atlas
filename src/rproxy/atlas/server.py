96f5c62c0b76c0987c68f24c02458ea42d3f2e6a
Pranshu Gupta <pranshug258@outlook.com>
2024-12-13 10:00:26 +0530

    added new props to server

--- src/rproxy/atlas/server.py
+++ src/rproxy/atlas/server.py
@@ -1,7 +1,10 @@
 class OriginServer:
-    def __init__(self, host, weight=1):
+    def __init__(self, host, weight=1, cpu=0, rif=0, latency=0):
         self.host = host
         self.weight = weight
+        self.cpu = cpu
+        self.rif = rif
+        self.latency = latency
 
     def __str__(self):
         return f"{self.host} ({self.weight})"
