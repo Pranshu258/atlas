752bfcc6b33371b849b497e0ee62491082495b9a
Pranshu Gupta <pranshug258@outlook.com>
2024-12-15 12:20:11 +0530

    added comment

--- src/rproxy/atlas/weightedroundrobinlb.py
+++ src/rproxy/atlas/weightedroundrobinlb.py
@@ -2,11 +2,27 @@ from .roundrobinlb import RoundRobinLoadBalancer
 from .originserver import OriginServer
 
 class WeightedRoundRobinLoadBalancer(RoundRobinLoadBalancer):
+    """
+    A load balancer that selects the origin server in a weighted round-robin fashion.
+    Servers with higher weights will be selected more frequently.
+    """
     def __init__(self, servers: dict[str, OriginServer] = None):
+        """
+        Initialize the weighted round-robin load balancer with the given servers.
+
+        Args:
+            servers (dict[str, OriginServer]): A dictionary of servers with hostnames as keys and OriginServer instances as values.
+        """
         super().__init__(servers)
         self.current_repeat_count = 0
         
     async def get_next_server(self):
+        """
+        Get the next server in a weighted round-robin fashion.
+        
+        Returns:
+            OriginServer: The next server to handle the request.
+        """
         host = self.hosts[self.current_index]
         async with self.lock:
             self.current_repeat_count += 1
