25ae587e1e6c361b6b3f658663cceaf6cfa7e8b0
Pranshu Gupta <pranshug258@outlook.com>
2024-12-19 10:27:47 +0530

    using the actual package

--- tests/rproxy/server/apps.py
+++ tests/rproxy/server/apps.py
@@ -1,9 +1,9 @@
 from django.apps import AppConfig
-import rproxy.atlas.src.loadbalancerfactory as LoadBalancerFactory
+from atlaslb.common.loadbalancerfactory import LoadBalancerFactory
 
 class ServerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'server'
-    loadbalancer = LoadBalancerFactory.LoadBalancerFactory().CreateLoadBalancer(
+    loadbalancer = LoadBalancerFactory().CreateLoadBalancer(
         {'algorithm': 'leastlatency', 'servers': None}
     )
