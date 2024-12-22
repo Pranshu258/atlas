ea674dfc790e65fdae6e855f74944d367bb74498
Pranshu Gupta <pranshug258@outlook.com>
2024-12-22 23:23:30 +0530

    added forwarding method to prequal algo

--- src/atlaslb/algorithms/prequallb.py
+++ src/atlaslb/algorithms/prequallb.py
@@ -1,4 +1,7 @@
 import random
+import aiohttp
+import time
+from django.http import HttpResponse
 from ..common.baselb import BaseLoadBalancer
 from ..common.originserver import OriginServer
 
@@ -15,7 +18,55 @@ class PrequalLoadBalancer(BaseLoadBalancer):
             servers (dict[str, OriginServer]): A dictionary of servers with hostnames as keys and OriginServer instances as values.
         """
         super().__init__(servers)
+        self.probes = []
+
+    async def forward_request(self, request, path: str):
+        """
+        Forward the request to the appropriate origin server based on the load balancing algorithm.
+        The algorithm is implemented in the derived classes. This method also maintains the requests-in-flight count and the exponential moving average of latency for each server.
+
+        Args:
+            request (HttpRequest): The incoming HTTP request.
+            path (str): The path to forward the request to.
+
+        Returns:
+            HttpResponse: The response from the origin server.
+        """
+        if not self.servers or len(self.servers) == 0:
+            return HttpResponse("Service unavailable, please try again after some time.", status=503)
         
+        origin = await self.get_next_server()
+        url = f"http://{origin.host}/{path}"
+
+        async with origin.lock:
+            origin.local_rif += 1
+         
+        request_start_time = time.time()
+        async with aiohttp.ClientSession(trust_env=True) as session: 
+            async with session.get(url) as response:
+                response = await response.text()
+        request_end_time = time.time()
+        elapsed_time = request_end_time - request_start_time
+
+        async with origin.lock:
+            origin.local_rif -= 1
+            origin.latency = (elapsed_time + origin.latency)/2
+
+        self.update_probe_pool(origin )
+
+        proxyResponse = HttpResponse(response)
+        proxyResponse["X-Atlas-Origin-Server"] = origin.host
+        return proxyResponse
+        
+    def update_probe_pool(self):
+        """
+        Update the probe pool with the latest latency and RIF values.
+        """
+        if len(self.probes) < 16:
+            self.probes = list(self.servers.values())
+            return
+        return None
+
     async def get_next_server(self):
         """
         Get the next origin server based on Google's Prequal algorithm.
